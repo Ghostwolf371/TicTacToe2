9662dd800eb31056b0ecef91ff38ccc93844b95f
Ghost Wolf <Kingbeast371@gmail.com>
2024-05-04 22:21:26 -0300

    Initial commit

---
+++ src/com/unasat/database/User.java
@@ -0,0 +1,9 @@
+package com.unasat.database;
+
+public class User {
+    public String name;
+    public String email;
+    public String phone;
+    public String address;
+    public String password;
+}
