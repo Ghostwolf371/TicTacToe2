176e41e177fba13f53fd88c9f02bb0a60c1181fa
Ghost Wolf <Kingbeast371@gmail.com>
2024-05-11 10:49:07 -0300

    RegistrationForm.form

--- src/com/unasat/model/Main.java
+++ src/com/unasat/model/Main.java
@@ -2,6 +2,6 @@ package com.unasat.model;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world!");
     }
 }
